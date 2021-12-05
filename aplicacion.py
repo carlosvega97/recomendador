6c0f9fece7cba7a478085f49c7ad048f01aecd16
joseIgnaciodv <joseignaciodelvallebustillo@gmail.com>
2021-12-05 20:53:40 +0100

    Interfaz de recomendaciones

---
+++ aplicacion.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import os
+import pandas as pd
+import operator
+
+#------------------------------------------ Funciones ----------------------------------------------------
+def extraer_etiquetas(ruta_noticia):
+    noticia = open(ruta_noticia, "r", encoding="utf8")      # Abrir y leer el fichero que se pasa por parametro
+    etiquetas = noticia.readlines()[-1]                     # Etiquetas de noticia estan en la ultima linea del fichero de texto
+    lista_etiquetas = etiquetas.split(",")                  # Se crea un lista con todas las etiquetas de la noticia
+    return lista_etiquetas
+
+def guardar_resultados(noticia_referencia, lista):
+    res = {}
+    etiquetas_noticia = extraer_etiquetas(noticia_referencia)
+    for noticia in lista:
+        lista_etiquetas = extraer_etiquetas(noticia)
+        res[noticia] = simlitud_dice(etiquetas_noticia, lista_etiquetas)
+    return res
+
+def mostrar_resultados(diccionario: dict):
+    lista_ranking = []
+    resultados = sorted(diccionario.items(), key=operator.itemgetter(1), reverse=True)
+    ranking = {k: v for k, v in resultados}
+    for noticia, similitud in ranking.items():
+        lista_ranking.append(str(noticia) + "- " + "Similitud: " + str(similitud))
+    return lista_ranking
+
+
+def simlitud_dice(etiquetas_texto1, etiquetas_texto2):
+    a = set(etiquetas_texto1)
+    b = set(etiquetas_texto2)
+
+    return (2*len(a.intersection(b)))/((len(a))+len(b))
+
+def mostrar_noticias(medio, categoria):
+    lista = []
+    for elemento in os.scandir("Noticias/" + medio + "/" + categoria + "/"):        # Escanear la carpeta del medio y categoria seleccionadas, devolver todo el contenido
+        lista.append(elemento.path)
+    return lista                                                                    # Devolver lista con todas las rutas de todos los ficheros de la carpeta
+
+def rastrear_directorio(medio): 
+    lista_noticias = []
+    for item in os.scandir("Noticias/" + medio):
+        carpeta = os.scandir(item)
+        for fichero in carpeta:
+            lista_noticias.append(fichero.path)
+    return lista_noticias 
+
+def busqueda(filtro):
+    lista = []
+    if filtro == "Todas":
+        pais = rastrear_directorio("El Pais")
+        mundo = rastrear_directorio("El Mundo")
+        minutos = rastrear_directorio("20 Minutos")
+        lista = pais + mundo + minutos
+    else: 
+        lista = rastrear_directorio(filtro)
+    return lista
+
+#----------------------------------------------- Interfaz Aplicacion ------------------------------------
+st.title("Seleccione una noticia de referencia")
+
+col1,col2,col3 = st.columns(3)
+
+with col1:
+    medio = st.selectbox("Medio", ["El Pais", "El Mundo", "20 Minutos"])
+with col2:
+    categoria = st.selectbox("Categoria", ["Tecnologia", "Ciencia", "Salud"])
+with col3:
+    noticia = st.selectbox("Noticia", mostrar_noticias(medio, categoria))
+
+texto_noticia = open(noticia, "r", encoding="utf8")
+st.text_area("Preview Noticia", texto_noticia.read())
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    n = st.selectbox("N Resultados: ", ["5", "7", "10"])
+
+col1, col2, col3 = st.columns(3)
+with col2:
+    st.write("")
+    buscar = st.button("Buscar")
+
+medio = st.selectbox("Filtro: ", ["Todas", "El Pais", "El Mundo", "20 Minutos"])
+
+#if buscar:
+lista_noticias = busqueda(medio)
+res = guardar_resultados(noticia, lista_noticias)
+lista_resultados = mostrar_resultados(res)
+resultados = st.selectbox("Ranking: ", lista_resultados[:int(n)])
+indice_guion = resultados.index("-")
+noticia_resultado = resultados[:indice_guion]
+noticia_resultado = open(noticia_resultado, "r", encoding="utf8")
+st.text_area("Noticia", noticia_resultado.read())
---
+++ guardar_noticias.py
@@ -0,0 +1,164 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import os
+
+def lista_urls_pais(url):
+    lista = [] 
+    html = requests.get(url)
+
+    web = BeautifulSoup(html.text, 'html.parser')
+    noticias = web.find_all("h2", class_="c_t")
+    
+
+    for url in noticias:
+        url_noticia = url.find('a').get('href')
+        url_noticia = "https://elpais.com" + url_noticia
+        lista.append(url_noticia)
+    return lista
+
+def lista_urls_20minutos(url):
+    lista = [] 
+    html = requests.get(url)
+
+    web = BeautifulSoup(html.text, 'html.parser')
+    noticias = web.find_all("div", class_="media-content")
+    
+    for url in noticias:
+        url_noticia = url.find('a').get('href')
+        lista.append(url_noticia)
+    return lista
+
+def lista_urls_elmundo(url):
+    lista = [] 
+    html = requests.get(url)
+
+    web = BeautifulSoup(html.text, 'html.parser')
+    noticias = web.find_all("a", class_="ue-c-cover-content__link-whole-content")
+    
+    
+    for url in noticias:
+        url_noticia = url.get('href')
+        lista.append(url_noticia)
+    return lista
+
+       
+lista_ciencia_pais = lista_urls_pais("https://elpais.com/ciencia/")
+lista_tecnologia_pais = lista_urls_pais("https://elpais.com/tecnologia/")
+lista_salud_pais = lista_urls_pais("https://elpais.com/noticias/salud/")
+
+lista_salud_20minutos = lista_urls_20minutos("https://www.20minutos.es/salud/")
+#lista_ciencia_20minutos = lista_urls_20minutos("https://www.20minutos.es/ciencia/")
+lista_tecnologia_20minutos = lista_urls_20minutos("https://www.20minutos.es/tecnologia/")
+
+lista_ciencia_elmundo = lista_urls_elmundo("https://www.elmundo.es/ciencia-y-salud/ciencia.html")
+lista_tecnologia_elmundo = lista_urls_elmundo("https://www.elmundo.es/tecnologia.html")
+lista_salud_elmundo = lista_urls_elmundo("https://www.elmundo.es/ciencia-y-salud/salud.html")
+
+
+def guardar_noticias_pais(lista_urls, tipo):
+    index = 0
+    for url in lista_urls:
+        html = requests.get(url)
+        web = BeautifulSoup(html.text, 'html.parser')
+        
+        try:
+            tags = web.find('meta', property="news_keywords").attrs['content']
+            titulo = web.find('h1', class_="a_t").text
+            entradilla = web.find('h2', class_="a_st").text
+            cuerpo = ""
+            
+            for para in web.find_all("p", class_=""):
+                cuerpo = cuerpo + para.get_text()
+                cuerpo = re.sub("Puedes seguir a EL PAÍS TECNOLOGÍA en Facebook y Twitter o apuntarte aquí para recibir nuestra newsletter semanal.", "", cuerpo)
+                cuerpo = re.sub("Suscríbete y lee sin límites", "", cuerpo)    
+            
+        except:
+            tags = ""
+            titulo = ""
+            entradilla = "" 
+        
+        noticia = titulo + "\n######\n" + entradilla + "\n######\n" + cuerpo + "\n######\n" + tags 
+        file = open("Noticias/El Pais/" + tipo + "/" + "El Pais_" + tipo + "_" + str(index) + ".txt", "w", encoding="utf-8")
+        file.write(noticia)
+        file.close()
+        index = index + 1
+        
+        
+def guardar_noticias_20minutos(lista_urls, tipo):
+    index = 0
+    for url in lista_urls:
+        html = requests.get(url)
+        web = BeautifulSoup(html.text, 'html.parser')
+        
+        try:
+            tags = web.find("meta",  {"name":"news_keywords"}).attrs['content']
+            titulo = web.find("meta",  {"property":"og:title"}).attrs['content']
+            entradilla = web.find('div', class_="article-intro").text
+            cuerpo= ""
+            for para in web.find_all('p', class_="paragraph"):
+                cuerpo = cuerpo + para.get_text()
+                cuerpo = re.sub("Apúntate a nuestra newsletter y recibe en tu correo las últimas noticias sobre tecnología.", "", cuerpo)             
+            
+        except:
+            tags = ""
+            titulo = ""
+            entradilla = "" 
+        
+        noticia = titulo + "\n######\n" + entradilla + "\n######\n" + cuerpo + "\n######\n" + tags  
+        file = open("Noticias/20 Minutos/" + tipo + "/" + "20 Minutos_" + tipo + "_" + str(index) + ".txt", "w", encoding="utf-8")
+        file.write(noticia)
+        file.close()
+        index = index + 1
+
+        
+def guardar_noticias_mundo(lista_urls, tipo):
+    index = 0
+    for url in lista_urls:
+        html = requests.get(url)
+        web = BeautifulSoup(html.text, 'html.parser')
+        
+        etiquetas = ""
+        tags = web.find_all('li', class_="ue-c-article__tags-item")
+        titulo = web.find('h1', class_="ue-c-article__headline js-headline")
+        entradilla = web.find('p', class_="ue-c-article__standfirst")
+
+        if titulo == None:
+            titulo = ""
+        else:
+            titulo = titulo.text
+
+        if entradilla == None:
+            entradilla = ""
+        else:
+            entradilla = entradilla.text
+        if tags == None:
+            tags = ""
+        else:
+            for t in tags:
+                etiquetas = etiquetas + ", " + t.get_text()
+
+        cuerpo= ""
+        for para in web.find_all('p', class_=""):
+            cuerpo = cuerpo + para.get_text()
+                             
+        noticia = titulo + "\n######\n" + entradilla + "\n######\n" + cuerpo + "\n######\n" + etiquetas  
+        file = open("Noticias/El Mundo/" + tipo + "/" + "El Mundo_" + tipo + "_" + str(index) + ".txt", "w", encoding="utf-8")
+        file.write(noticia)
+        file.close()
+        index = index + 1
+
+
+guardar_noticias_pais(lista_tecnologia_pais, "Tecnologia")
+guardar_noticias_pais(lista_ciencia_pais, "Ciencia")
+guardar_noticias_pais(lista_salud_pais, "Salud")
+
+guardar_noticias_20minutos(lista_tecnologia_20minutos, "Tecnologia")
+#guardar_noticias_20minutos(lista_ciencia_20minutos, "_ciencia_20minutos_")
+guardar_noticias_20minutos(lista_salud_20minutos, "Salud")
+
+guardar_noticias_mundo(lista_tecnologia_elmundo, "Tecnologia")
+guardar_noticias_mundo(lista_ciencia_elmundo, "Ciencia")
+guardar_noticias_mundo(lista_salud_elmundo, "Salud")
+ 
+ 
